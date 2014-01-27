7675c0c022c30c6b9a9af06b115c3070ea1542ad
Geoff Goehle <goehle@gmail.com>
2014-01-27 16:51:58 -0500

    Bringing test mathpets back online.

--- lib/WeBWorK/MathPet.pm
+++ lib/WeBWorK/MathPet.pm
@@ -40,7 +40,7 @@ sub getPetData {
     }
 
     my $points = $globalUserAchievement->achievement_points;
-    my $url = $ce->{webworkURLs}->{htdocs}.'/js/apps/MathPet/img/cat.gif';
+    my $url = $ce->{webworkURLs}->{htdocs}.'/js/apps/MathPet/img/sprite_derpy.gif';
 
     my $script = <<EOS;     
     var myPet = new mathPet($points,"$url");
